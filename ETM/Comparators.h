51fea4bd4f17901f16fc29832ca041fafbb8b255
Yusuf Nasser <yesyosife@gmail.com>
2021-06-03 11:12:28 +0200

    Added Comparators.h as the soring bases
    Co-authored-by: Badr-1 <badrmohamedashraf@gmail.com>
    Co-authored-by: AhmeddHassann <ahmedhassan10120@gmail.com>
    Co-authored-by: aymanred121 <ayman.king355@gmail.com>

---
+++ ETM/Comparators.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Task.h"
+
+struct ComparePriority {
+    bool operator()(Task const& t1, Task const& t2)
+    {
+        if (t1.getPriority() < t2.getPriority()) {
+            return true;
+        }
+        else if (t1.getPriority() == t2.getPriority()) {
+            if (t1.getDeadline() > t2.getDeadline()) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+struct CompareDeadline {
+    bool operator()(Task const& t1, Task const& t2)
+    {
+        if (t1.getDeadline() > t2.getDeadline()) {
+            return true;
+        }
+        else if (t1.getDeadline() == t2.getDeadline()) {
+            if (t1.getPriority() > t2.getPriority()) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
